5db776dc919d69b9f9f7628f52b78567f505b14b
Batın Evirgen <batinevirgen@gmail.com>
2023-03-03 13:24:58 +0300

    feat: add all authors page

---
+++ src/pages/author/index.tsx
@@ -0,0 +1,40 @@
+import Article from '@/components/Article';
+import { BlogAuthor, getAllAuthors } from '@/lib/blog';
+import { toReadableDate } from '@/lib/utils/date';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+
+interface PageProps {
+  authors: BlogAuthor[];
+}
+
+export default function Page({ authors }: PageProps) {
+  return (
+    <div>
+      <h1>All Authors</h1>
+      {authors.map((author) => (
+        <Article key={author.slug}>
+          <Article.Header>{author.name}</Article.Header>
+          <Article.Body>
+            <p>
+              Joined at <time>{toReadableDate(author.createdAt)}</time>
+            </p>
+          </Article.Body>
+          <Article.Footer>
+            <Link href={`/author/${author.slug}`}>See all posts by {author.name}</Link>
+          </Article.Footer>
+        </Article>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PageProps> = async ({ params }) => {
+  const authors = await getAllAuthors('local');
+
+  return {
+    props: {
+      authors,
+    },
+  };
+};
