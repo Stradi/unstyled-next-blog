b51134bf01ea78aedcb97bff1137ac883516190c
Batın Evirgen <batinevirgen@gmail.com>
2023-03-03 13:26:11 +0300

    feat: add all tags page

---
+++ src/pages/tag/index.tsx
@@ -0,0 +1,40 @@
+import Article from '@/components/Article';
+import { BlogTag, getAllTags } from '@/lib/blog';
+import { toReadableDate } from '@/lib/utils/date';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+
+interface PageProps {
+  tags: BlogTag[];
+}
+
+export default function Page({ tags }: PageProps) {
+  return (
+    <div>
+      <h1>All Tags</h1>
+      {tags.map((tag) => (
+        <Article key={tag.slug}>
+          <Article.Header>{tag.name}</Article.Header>
+          <Article.Body>
+            <p>
+              Joined at <time>{toReadableDate(tag.createdAt)}</time>
+            </p>
+          </Article.Body>
+          <Article.Footer>
+            <Link href={`/tag/${tag.slug}`}>See all posts tagged with {tag.name}</Link>
+          </Article.Footer>
+        </Article>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PageProps> = async ({ params }) => {
+  const tags = await getAllTags('local');
+
+  return {
+    props: {
+      tags,
+    },
+  };
+};
