ff523c5b5374cbedb4de4bf8cc266ba1305ec78f
Batın Evirgen <batinevirgen@gmail.com>
2023-03-05 16:10:30 +0300

    fix: remove the asterisk from rewrite rule because im dumb

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
         destination: '/page/index',
       },
       {
-        source: '/:path*',
-        destination: '/page/:path*',
+        source: '/:path',
+        destination: '/page/:path',
       },
     ];
   },
