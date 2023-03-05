9080fdbbf7c7abd23ee6014719030e8bcd2fc9d1
Batın Evirgen <batinevirgen@gmail.com>
2023-03-05 13:25:50 +0300

    fix: add rewrite for static pages to simplify `src/pages` folder
    Moving `src/pages/[...slug].tsx` page to `src/pages/page/[...slug].tsx`
    file and adding rewrite for static pages to simplify `src/pages` folder.
    
    By using Next.js's rewrite feature we mapped `/` -> `/page/index` and
    `/any-page` -> `/page/page` and so on. So basically nothing has changed
    for the user, but we simplified the `src/pages` folder.

---
+++ _content/pages/index/index.md
@@ -0,0 +1,9 @@
+---
+name: 'Index'
+description: 'Index page of Unstyled Next Blog.'
+image:
+  src: 'cover.webp'
+  alt: 'A picture containing nothing, I guess.'
+---
+
+Unstyled Next Blog is Awesome!
---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/page/index',
+      },
+      {
+        source: '/:path*',
+        destination: '/page/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
